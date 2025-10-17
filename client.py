c9e9b53f3945db8010929168e2996045b9ba2b5e
hanifa69 <haanifaa.69@gmail.com>
2025-10-17 14:05:39 -0700

    "Revised code"

--- client.py
+++ client.py
@@ -1,14 +1,35 @@
 import socket
-def send(message):
-    s.sendall(message.encode())
-    data = s.recv(1024)
-    print(data.decode())
+import threading
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect(("127.0.0.1" , 60000))
-
-    user_name=input("please enter username:").send()
-
+def recv(conn):
     while True:
-        message = input("please enter message: ")
-        send (message)
+        try:
+            data =conn.recv(1024)
+            if not data:
+                print("disconection for server..")
+                break
+            print(data.decode())
+        except:
+            print("Error")
+            break
+
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect(("127.0.0.1", 60000))
+        username = input("Please enter username: ")
+        s.sendall(username.encode())
+
+        # Thread جدا برای دریافت پیام ها
+        t = threading.Thread(target=recv, args=(s,))
+        t.start()
+        while True:
+            try:
+                message= input()
+                s.sendall(message.encode())
+            #except KeyboardInterrupt:
+             #   print("Interrupted by user")
+              #  break
+            except Exception as e:
+                print(f"Error sending message: {e}")
+                break
+main()
--- server.py
+++ server.py
@@ -7,25 +7,39 @@ def connection(conn):
     global members
     with conn:
         #conn.sendall(b"Welcome! Type something:\n")
+     #   while True:
+        user_name= conn.recv(1024).decode('utf-8')
+
+        members.append(conn)
+        user_names.append(user_name)
+
+        message_join=" joined the chat room"
+        send(user_name,message_join,conn)
         while True:
-            user_name= conn.recv(1024)
-
-            members.append(conn)
-            user_names.append(user_name)
-
-            message_join="joined the chat room"
-            send(user_name,message_join,conn)
+            try:
+                message = conn.recv(1024).decode('utf-8')
+                if not message:
+                    break
+                if message =="list":
+                    message= user_names
+                send(user_name,message, conn)
+            except:
+                break
+        #index = members.index(conn)
+        #members.remove(conn)
+        #left_user = user_names.pop(index)
+        #send( left_user, 'left the chat room.', conn)
             
 def send (user_name,message,conn):
     global members
     for client in members:
-        try:
-            if client != conn:
-                messages=user_name+message
-                print(messages)
-                client.sendall(messages.encode())
-        except:
-            print("eror")    
+              
+            time_now=str(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
+            messages=user_name+":"+message+"\n"+time_now
+
+            print(messages)
+            client.sendall(messages.encode('utf-8'))
+            
 def many():
        
     HOST= '127.0.0.1'
